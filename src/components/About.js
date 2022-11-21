161b7bcb9208d565b6ba7c50048c07d2f09c94b8
Raif Coonjah <raifcoonjah@pm.me>
2022-11-21 10:03:14 +0400

    feat: Add download links
    * Added buttons.
    * Remove gradient background.
    
    Signed-off-by: Raif Coonjah <raifcoonjah@pm.me>

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <section id="about">
-      <div className="bg-gradient-to-r via-gray-900 from-gray-700  to-black container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="font-bold title-font sm:text-4xl text-3xl mb-4  text-white">
             Saturn startpage
@@ -13,23 +13,24 @@ export default function About() {
           </p>
           <div className="flex justify-center">
             <a
-              href="https://github.com/raifcoonjah/saturn-startpage/releases"
+              href="https://github.com/raifcoonjah/Saturn-startpage/releases/latest"
               className="py-2.5 px-5 mr-4 mb-4 text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
             >
-              Get for Firefox
+              Get for Firefox (.xpi)
+            </a>
+            <a
+              href="https://github.com/raifcoonjah/Saturn-startpage/releases/latest"
+              className="py-2.5 px-5 mr-4 mb-4 text-white bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-900"
+            >
+              Get for Chrome (.zip)
             </a>
             <a
               href="https://demo.saturn-startpage.netlify.app"
               target="_blank"
+              rel="noopener noreferrer"
               className="py-2.5 px-5 mr-4 mb-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
             >
               Try demo
-            </a>
-            <a
-              href="https://github.com/raifcoonjah/saturn-startpage/releases"
-              className="py-2.5 px-5 mr-4 mb-4 text-white font-medium text-gray-300"
-            >
-              Other versions?
             </a>
           </div>
         </div>
